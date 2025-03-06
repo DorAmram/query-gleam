330a39ee10659ab4d469f8aebb373e3c2273bb9f
Dor Amarm <doramram210@gmail.com>
2025-03-06 08:15:53 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/query-gleam/',
   server: {
     host: "::",
     port: 8080,
