dc716ae36a294a3ceb885960075fd8d77889ed73
Dor Amram <dor.amram@similarweb.com>
2025-03-06 11:15:04 +0200

    fixes

--- src/pages/ViewSurvey.tsx
+++ src/pages/ViewSurvey.tsx
@@ -178,19 +178,17 @@ const ViewSurvey = () => {
             if (!answer) return null;
             
             let content;
-            let selectedChoice;
-            let selectedChoices;
             
             switch (currentQuestion.type) {
               case 'text':
                 content = <p className="mb-2">{answer.value as string}</p>;
                 break;
               case 'multipleChoice':
-                selectedChoice = currentQuestion.choices?.find(c => c.id === answer.value);
+                const selectedChoice = currentQuestion.choices?.find(c => c.id === answer.value);
                 content = <p className="mb-2">{selectedChoice?.text}</p>;
                 break;
               case 'checkbox':
-                selectedChoices = currentQuestion.choices?.filter(c => 
+                const selectedChoices = currentQuestion.choices?.filter(c => 
                   (answer.value as string[]).includes(c.id)
                 );
                 content = (
